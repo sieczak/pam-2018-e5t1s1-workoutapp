c4d9206a013219d42fb65a9b90188e84079d7273
MXT2356\MXT2356 <aros244@gmail.com>
2018-06-17 23:39:00 +0200

    latest update

--- app/src/main/java/com/example/paulina/workoutapplication/MainActivity.java
+++ app/src/main/java/com/example/paulina/workoutapplication/MainActivity.java
@@ -6,10 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.List;
-import java.util.TooManyListenersException;
-
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this, notes.class);
+                Intent intent = new Intent(MainActivity.this, Notes.class);
                 startActivity(intent);
 
             }
